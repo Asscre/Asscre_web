fa4b0464a2e6526e08cad67736e83dca427ae665
huangjiahong <lj1314jh>
2020-12-07 16:24:28 +0800

    保存代码

---
+++ src/pages/body/log_page.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "log_page"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/body/me_page.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "me_page"
+}
+</script>
+
+<style scoped>
+
+</style>
